8e8d71dc80ba5db1117eca8ec8e8d370ca5756dc
Jan Pfrang <jan.pfrang@delonghigroup.com>
2026-05-21 10:38:56 +0200

    Update PZEM004Tv30.h

--- tests/native/mocks/PZEM004Tv30.h
+++ tests/native/mocks/PZEM004Tv30.h
@@ -1,14 +1,2 @@
-#ifndef MOCK_PZEM004T_H
-#define MOCK_PZEM004T_H
-
-#include "Arduino.h"
-
-class PZEM004Tv30 {
-public:
-    PZEM004Tv30(MockSerial& serial, int rx, int tx) {}
-    float voltage() { return 230.0f; }
-    float power() { return 1500.0f; }
-    float pf() { return 0.98f; }
-};
-
-#endif
+#pragma once
+#include "../arduino_mock.h"
