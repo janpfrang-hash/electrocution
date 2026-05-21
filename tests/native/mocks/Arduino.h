952e4e28bc3b32d39ceb16b8331f6a5649ca97c1
Jan Pfrang <jan.pfrang@delonghigroup.com>
2026-05-21 10:38:24 +0200

    Update Arduino.h

--- tests/native/mocks/Arduino.h
+++ tests/native/mocks/Arduino.h
@@ -1,25 +1,3 @@
-#ifndef MOCK_ARDUINO_H
-#define MOCK_ARDUINO_H
-
-#include <cstdint>
-#include <cmath>
-#include <iostream>
-
-#define VSPI 3
-#define INPUT_PULLUP 2
-#define INPUT 1
-
-inline uint32_t millis() { return 0; }
-inline void delay(uint32_t ms) {}
-
-class MockSerial {
-public:
-    template<typename... Args>
-    void printf(const char* format, Args... args) {}
-    void println(const char* str) {}
-};
-
-extern MockSerial Serial;
-extern MockSerial Serial2;
-
-#endif
+#pragma once
+// Forward all dependencies to your central mock file
+#include "../arduino_mock.h"
