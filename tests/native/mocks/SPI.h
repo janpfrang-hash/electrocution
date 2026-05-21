b48a0523af580043df7fd6ea9175ab48a2a3bdad
Jan Pfrang <jan.pfrang@delonghigroup.com>
2026-05-21 10:39:20 +0200

    Update SPI.h

--- tests/native/mocks/SPI.h
+++ tests/native/mocks/SPI.h
@@ -1,10 +1,2 @@
-#ifndef MOCK_SPI_H
-#define MOCK_SPI_H
-
-class SPIClass {
-public:
-    SPIClass(int bus) {}
-    void begin(int clk, int miso, int mosi, int ss) {}
-};
-
-#endif
+#pragma once
+#include "../arduino_mock.h"
